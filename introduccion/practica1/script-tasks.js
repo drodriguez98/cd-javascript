b979b10a687f9c2dddd7d0ecb68ddd85e49718ee
Diego Rodriguez <diego25598@gmail.com>
2023-10-30 14:37:12 +0100

    Add new js exercises

--- introduccion/practica1/script-tasks.js
+++ introduccion/practica1/script-tasks.js
@@ -1,6 +1,4 @@
 function agregarTarea() {
-
-    // RECOGIDA DE DATOS EN FORMULARIO Y VALIDACIÓN.
     
     // Obtiene el valor del campo de entrada por su id y verifica que no es nulo.
 
@@ -14,20 +12,18 @@ function agregarTarea() {
 
     }
 
-    // CREACIÓN DE ELEMENTOS.
-
     // Crea un nuevo elemento li y lo asigna a la clase "tarea".
 
     var newTask = document.createElement("li");
     newTask.className = "tarea";
 
-    // Crea un elemento input de tipo "checkbox" y lo asigna a la clase "checkbox".
+    // Crea un elemento input de tipo "checkbox" para marcar cada tarea como pendiente o completada y lo asigna a la clase "checkbox".
 
     var checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.className = "checkbox";
 
-    // Crea un botón "Eliminar", le asigna el texto "Eliminar" y lo asocia a una función que se encarga de borrar la tarea.
+    // Crea un botón asociado a una función que se encarga de borrar cada tarea
 
     var newDeleteButton = document.createElement("button");
     newDeleteButton.textContent = "Eliminar";
@@ -39,9 +35,7 @@ function agregarTarea() {
     newTask.appendChild(document.createTextNode(textTask));
     newTask.appendChild(newDeleteButton);
 
-    // EVENTO DE ESCUCHA.
-
-    // Agregar un evento de escucha para los checkboxes. Mover tarea completada a la sección correspondiente según el valor del checkbox.
+    // Agrega un evento de escucha para los checkboxes. Mueve las tareas a la sección de pendientes o completadas según el valor del checkbox.
 
     checkbox.addEventListener("change", function() {
 
@@ -58,16 +52,16 @@ function agregarTarea() {
         newTask.style.textDecoration = "none";
 
       }
-    });
 
-    // AGREGAR LA TAREA A LA LISTA Y LIMPIAR EL CAMPO DE ENTRADA DEL FORMULARIO.
+    });
 
     // Agrega la tarea a la lista de tareas pendientes.
 
     var pendingTasksList = document.getElementById("lista-pendientes");
     pendingTasksList.appendChild(newTask);
 
-    // Limpiar el campo de entrada de texto.
+    // Limpia el campo de entrada de texto.
 
     inputTask.value = "";
+
 }
