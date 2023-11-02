c032002f9846e78b4514068c4c8a552c98637a49
Diego Rodriguez <diego25598@gmail.com>
2023-11-02 10:01:36 +0100

    Add new exercises

--- practica5/script-images-2.js
+++ practica5/script-images-2.js
@@ -1,37 +1,38 @@
-// Lleva un registro del índice de la imagen actual
+let currentIndex = 0;
 
-var currentIndex = 1; 
-
-// Construye el nombre del archivo de la imagen y actualiza la fuente de la misma
+const galleryImages = document.querySelectorAll('.galleryImage');
 
 function showImage(index) {
 
-  var imgName = "img" + index + ".jpg";
+    galleryImages.forEach((image, i) => {
 
-  document.getElementById("miImagen").src = imgName;
+      if (i === index) {
 
-}
+        image.style.display = 'block';
+
+      } else {
+
+        image.style.display = 'none';
+
+      }
+
+    });
+
+  }
+  
 
 function previous() {
 
-  currentIndex--;
-
-  if (currentIndex < 1) { currentIndex = 3; }
-
+  currentIndex = (currentIndex - 1 + galleryImages.length) % galleryImages.length;
   showImage(currentIndex);
 
 }
 
 function next() {
 
-  currentIndex++;
-
-  if (currentIndex > 3) { currentIndex = 1; }
-
+  currentIndex = (currentIndex + 1) % galleryImages.length;
   showImage(currentIndex);
-
+  
 }
-
-// Llamamos a showImage para mostrar la imagen actual al cargar la página
 
 showImage(currentIndex);
